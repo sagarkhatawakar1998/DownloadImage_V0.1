a7939dcdf8df131d40e24e59f1ba2c5963777a55
nandan upadhyay <nandanupadhyay@nandans-Mac-mini.local>
2021-01-28 19:01:53 +0530

    First Commit

---
+++ DownloadImage/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DownloadImage
+//
+//  Created by nandan upadhyay on 28/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        imagevi() 
+    }
+
+    func imagevi() {
+        print("hi")
+    }
+}
+
